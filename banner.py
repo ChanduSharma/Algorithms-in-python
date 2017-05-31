12bde0aea44d01b418606e7b1fde522191da1696
ChanduSharma <chandu.rikku@gmail.com>
2017-05-31 15:59:19 +0530

    Update banner.py

--- banner.py
+++ banner.py
@@ -158,7 +158,7 @@ z = ('#########',
      '#########')
 	 
 	 
-#A dictionary which maps character to their tuples.	 
+#A dictionary which maps character to their corresponding display tuples.	 
 mylist = {'a':a,'b':b,'c':c,'d':d,'e':e,'f':f,'g':g,'h':h,
 	  'i':i,'j':j,'k':k,'l':l,'m':m,'n':n,'o':o,'p':p,
 	  'q':q,'r':r,'s':s,'t':t,'u':u,'v':v,'w':w,'x':x,
@@ -167,21 +167,22 @@ mylist = {'a':a,'b':b,'c':c,'d':d,'e':e,'f':f,'g':g,'h':h,
 #first argument of the command line
 #So it will not read space separated name in command line
 if len(sys.argv) > 1:
-	name = sys.argv[1];
+	name_list = sys.argv[1:];
 	itr = 0 
 	coloumn = 0
-	while itr < 5:
-	#Iteration is taken as five because the display I used 
-	# is a 5 segment display tuple.
-		line = []
+	for name in name_list:
+		while itr < 5:
+		#Iteration is taken as five because the display I used 
+		# is a 5 segment display tuple.
+			line = []
 	
-		for i in name.lower():
-			line.append(mylist[i][coloumn])
+			for i in name.lower():
+				line.append(mylist[i][coloumn])
 	
-		coloumn += 1
-		print('  '.join(line))
+			coloumn += 1
+			print('  '.join(line))
 	
-		itr += 1
+			itr += 1
 else:
 	print('Usage : banner.py [name]')
 	print('And Yeah!Ofcourse name without brackets.')
